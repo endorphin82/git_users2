9529e0db53b52e8092534565b50dcc3b922737f5
endorphin82 <endorphin82@gmail.com>
2020-02-03 10:47:07 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.432e4fbc1d0f06324718680838f4655e.js"
+  "./precache-manifest.652e0149acb693239cf9655249dd76d4.js"
 );
 
 self.addEventListener('message', (event) => {
